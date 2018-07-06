f2e28656b2d983c042b0a19ef30b6b2f47d20ed6
Ervandra Halim <ervandra.halim@gmail.com>
2018-07-06 16:40:33 +0700

    Update Tablet & Mobile version

--- js/app.js
+++ js/app.js
@@ -1,4 +1,31 @@
 var jQ = jQuery.noConflict();
 jQ(document).ready(function($){
   $(document).foundation();
+  $('body').addClass('is-ready');
+  
+  $('#fullpage').fullpage({
+    fixedElements: '#header',
+    navigation: true,
+    normalScrollElementTouchThreshold: 2,
+    css3: true,
+    menu: '#menu-stream',
+    responsiveWidth: 801,
+    responsiveHeight: 601,
+    scrollingSpeed: 1000,
+    bigSectionsDestination: 'top',
+    slidesNavigation: false,
+    onLeave: function (index, nextIndex, direction) {
+      var leavingSection = this;
+      if ( nextIndex == 3 ) {
+        $('#menu-stream li[data-menuanchor="intro"] a').addClass('active-by-js')
+      } else if (nextIndex != 3) {
+        $('#menu-stream li[data-menuanchor="intro"] a').removeClass('active-by-js')
+      }
+      if ( nextIndex == 6 || nextIndex == 7 ) {
+        $('#menu-stream li[data-menuanchor="cloud-gaming"] a').addClass('active-by-js')
+      } else if ( nextIndex != 6 || nextIndex != 7 ) {
+        $('#menu-stream li[data-menuanchor="cloud-gaming"] a').removeClass('active-by-js')
+      }
+    }
+  });
 })
