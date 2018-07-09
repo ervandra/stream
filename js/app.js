7f47b4523dfd127aaf2b5bd223ddcadcfb5a7285
Ervandra Halim <ervandra.halim@gmail.com>
2018-07-09 11:26:12 +0700

    active menu indicator

--- js/app.js
+++ js/app.js
@@ -14,8 +14,32 @@ jQ(document).ready(function($){
     scrollingSpeed: 1000,
     bigSectionsDestination: 'top',
     slidesNavigation: false,
+    afterLoad: function(origin, index, destination, direction){
+      if ( index == 2 || index == 3 ) { 
+        $('#fp-nav ul li:nth-child(2), #fp-nav ul li:nth-child(3)').addClass('same-section')
+      }else {
+        $('#fp-nav ul li:nth-child(2), #fp-nav ul li:nth-child(3)').removeClass('same-section')
+      }
+      if ( index == 5 || index == 6 || index == 7 ) { 
+        $('#fp-nav ul li:nth-child(5), #fp-nav ul li:nth-child(6), #fp-nav ul li:nth-child(7)').addClass('same-section')
+      }else {
+        $('#fp-nav ul li:nth-child(5), #fp-nav ul li:nth-child(6), #fp-nav ul li:nth-child(7)').removeClass('same-section')
+      }
+
+    },
     onLeave: function (index, nextIndex, direction) {
       var leavingSection = this;
+      if ( nextIndex == 2 || nextIndex == 3 ) { 
+        $('#fp-nav ul li:nth-child(2), #fp-nav ul li:nth-child(3)').addClass('same-section')
+      }else {
+        $('#fp-nav ul li:nth-child(2), #fp-nav ul li:nth-child(3)').removeClass('same-section')
+      }
+      if ( nextIndex == 5 || nextIndex == 6 || nextIndex == 7 ) { 
+        $('#fp-nav ul li:nth-child(5), #fp-nav ul li:nth-child(6), #fp-nav ul li:nth-child(7)').addClass('same-section')
+      }else {
+        $('#fp-nav ul li:nth-child(5), #fp-nav ul li:nth-child(6), #fp-nav ul li:nth-child(7)').removeClass('same-section')
+      }
+
       if ( nextIndex == 3 ) {
         $('#menu-stream li[data-menuanchor="intro"] a').addClass('active-by-js')
       } else if (nextIndex != 3) {
